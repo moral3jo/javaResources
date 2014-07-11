f31d0540bbb6d311aa714b0b762d58cfd3c0aafe
Roberto <moral3jo@gmail.com>
2014-07-11 12:42:09 +0200

    Update 02-Controlador.java

--- gestionErrores/ajax/02-Controlador.java
+++ gestionErrores/ajax/02-Controlador.java
@@ -1,1 +1,23 @@
-0
+package com.proyecto.front.controller;
+
+@RequestMapping("/Modulo/**")
+@Controller
+public class Modulo extends BaseController{
+  
+  /*
+   * Lanzamos la exception pero no llegará a la web. Es capturada desde BaseController
+   */
+  @RequestMapping(value = "/llamadaAjax")
+  public ModelAndView showForm(HttpServletRequest request, ModelMap model) throws AccesoServiciosException {
+    return servidor.llamada();
+  }
+  
+  
+  @ExceptionHandler(AccesoServiciosException.class)
+	@ResponseBody
+	public void handleAjaxAccesoServiciosException(AccesoServiciosException ex, HttpServletResponse response){		
+		returnAjaxError(ex, response);
+
+	}
+
+}
