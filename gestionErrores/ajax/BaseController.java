2bf211d5c745ec2dbdde97aa03165a94607e83ec
Roberto <moral3jo@gmail.com>
2014-07-11 12:51:29 +0200

    Update BaseController.java

--- gestionErrores/ajax/BaseController.java
+++ gestionErrores/ajax/BaseController.java
@@ -1,5 +1,31 @@
 package com.proyecto.front.controller;
 
 public class BaseController{
+		PROTECTED
   
+
+	protected void returnAjaxError(AccesoServiciosException exception, HttpServletResponse response) {
+		String detalleMensaje = obtieneError(exception);
+		devolverErrorWeb(detalleMensaje);
+	}
+    
+	private String obtieneError(AccesoServiciosException exception){
+		//Podemos validar tipo de excepcion original
+		if( ex.getCause() instanceof ServerWebApplicationException ){
+		  ServerWebApplicationException swaEx = (ServerWebApplicationException)ex.getCause();
+		  detalleMensaje = getServerWebApplicationExceptionError( swaEx );
+		}else{
+		  return textoGenericoError();
+		}
+	}
+	
+	private devolverErrorWeb(String detalleMensaje){
+	  //response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		try {
+			response.getWriter().write(detalleMensaje);
+		} catch (IOException e) {
+			LOGGER.info(Arrays.toString(e.getStackTrace()) );
+		}
+	}
+	
 }
